294507a360b7335eb9a45dff2679f0442e1d357d
Tyler Strunk <wtstrunk1@gmail.com>
2024-02-08 15:36:48 -0500

    The form is now working, accepting the data and storing it in an object. There is also some code to make sure the passwords match. If passwords don't match an error is thrown. The only issue is that since the create account button is positioned relatively to the rows in the form, the button moves when the error message appears. I need to find a way around this. Maybe I can position the error message absolutely to keep it from effecting the flow of anything else? I will investigate.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+
+
+
+
+document.getElementById('duckForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
   
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone')
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
     const errorElement = document.getElementById('error');
   
     if (password !== confirmPassword) {
-      errorElement.style.display = 'block'; // Display error message
-      document.getElementById('password').classList.add('invalid'); // Highlight password field
-      document.getElementById('confirmPassword').classList.add('invalid'); // Highlight confirm password field
+      errorElement.style.display = 'block'; 
+      document.getElementById('password').classList.add('invalid'); 
+      document.getElementById('confirmPassword').classList.add('invalid'); 
     } else {
-      errorElement.style.display = 'none'; // Hide error message
-      document.getElementById('password').classList.remove('invalid'); // Remove highlight from password field
-      document.getElementById('confirmPassword').classList.remove('invalid'); // Remove highlight from confirm password field
-      // Form submission logic here (e.g., submit the form)
+      errorElement.style.display = 'none'; 
+      document.getElementById('password').classList.remove('invalid'); 
+      document.getElementById('confirmPassword').classList.remove('invalid'); 
     }
+
+    
+
+    const formData = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phone: phone,
+        password: password
+      };
+  
   });
